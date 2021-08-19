052f57f2995791a8d6d63aa9732de5d1f39c4b13
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-08-19 01:39:57 -0700

    Updated API & cfg

--- src/LastFmApi.py
+++ src/LastFmApi.py
@@ -1,126 +1,192 @@
-
-from time import time, sleep
 import requests
+import yaml
+import time
+import sys
 
-USE_CACHE = False
+#=============================================-#
+#----------------| API config |----------------#
+#==============================================#
 
-if USE_CACHE:
-    import requests_cache
-    requests_cache.install_cache()
+API_URL       = ''
+API_KEY       = ''
+HEADERS       = {}
+USE_CACHE     = False
+CALL_INTERVAL = 1
 
-API_URL   = 'http://ws.audioscrobbler.com/2.0/'
-CALL_RATE = 5 # calls per second
-CALL_INT  = 1 / CALL_RATE # interval
+ready = False
+lastrequesttime = None
 
-import ApiConfig
-#
-# Inside ApiConfig.py: Define KEY string
-#
-API_KEY = ApiConfig.KEY
+def init(cfg_fn):
+    """
+    Set up LastFmApi using api_cfg.yaml file
+    """
+    global API_URL
+    global API_KEY
+    global HEADERS
+    global USE_CACHE
+    global CALL_INTERVAL
+    global ready
 
+    with open(cfg_fn, 'r') as f:
+        cfg = yaml.load(f)
+    
+    API_URL    = cfg['api_url']
+    API_KEY    = cfg['api_key']
+    USER_AGENT = cfg['user_agent']
+    USE_CACHE  = cfg['use_cache']
+    CALL_RATE  = cfg['call_rate']
 
-# Interface class for requesting data from the last.fm API
-class LastFmApi:
-    # Initializes request data
-    def __init__(self, userAgent, format='json'):
-        self.key         = API_KEY
-        self.headers     = { 'user_agent': userAgent }
-        self.format      = format
-        self.lastApiCall = None
+    HEADERS = { 'user_agent': USER_AGENT }
 
-    # Appends API key and format to the payload and returns the formatted API Response
-    def get_response(self, payload):
-        payload['api_key'] = self.key
-        payload['format']  = self.format
+    if USE_CACHE:
+        try:
+            import requests_cache
+        except ImportError:
+            sys.exit('use_cache=True and requests_cache could not be imported.')
+        
+        requests_cache.install_cache()
 
-        self.rate_limiter()
-        response = requests.get(API_URL, headers=self.headers, params=payload)
+    CALL_INTERVAL = 1 / CALL_RATE
 
-        if response.status_code != requests.codes.ok:
-            print(response.text)
-            return None
+    ready = True
 
-        if USE_CACHE and 'from_cache' not in response:
-            self.lastApiCall = time() # Updates time of last API call
+#======================================================-#
+#----------------| API method wrappers |----------------#
+#=======================================================#
 
-        if self.format == 'json':
-            return response.json()
-        else:
-            return response
+def get_user_info(self, user):
+    """
+    user.getInfo
+    """
+    payload = {
+        'method' : 'user.getInfo',
+        'user'   : user
+    }
+    return __get_response(payload)
 
-    # Waits until the required interval between API calls is reached
-    def rate_limiter(self):
-        if self.lastApiCall is not None: # If there was a previous call to the API
-            timeSince = time() - self.lastApiCall
-            if timeSince < CALL_INT:
-                sleep(CALL_INT - timeSince)
+def get_user_recent_tracks(self, user, limit=50, page=1):
+    """
+    user.getRecentTracks
+    """
+    payload = {
+        'method' : 'user.getRecentTracks',
+        'user'   : user,
+        'limit'  : limit,
+        'page'   : page
+    }
+    return __get_response(payload)
 
-    # API Method Wrappers
+def get_user_top_artists(self, user, limit=50, page=1):
+    """
+    user.getTopArtists
+    """
+    payload = {
+        'method' : 'user.getTopArtists',
+        'user'   : user,
+        'limit'  : limit,
+        'page'   : page
+    }
+    return __get_response(payload)
 
-    def user_info(self, user):
-        payload = { 'method': 'user.getInfo',
-                    'user'  : user
-        }
-        return self.get_response(payload)
+def get_user_top_albums(self, user, limit=50, page=1):
+    """
+    user.getTopAlbums
+    """
+    payload = {
+        'method' : 'user.getTopAlbums',
+        'user'   : user,
+        'limit'  : limit,
+        'page'   : page
+    }
+    return __get_response(payload)
 
-    def user_recent_tracks(self, user, limit=50, page=1):
-        payload = { 'method': 'user.getRecentTracks',
-                    'user'  : user,
-                    'limit' : limit,
-                    'page'  : page
-        }
-        return self.get_response(payload)
+def get_user_top_tracks(self, user, limit=50, page=1):
+    """
+    user.getTopTrack
+    """
+    payload = {
+        'method' : 'user.getTopTracks',
+        'user'   : user,
+        'limit'  : limit,
+        'page'   : page
+    }
+    return __get_response(payload)
 
-    def user_top_artists(self, user, limit=50, page=1):
-        payload = { 'method': 'user.getTopArtists',
-                    'user'  : user,
-                    'limit' : limit,
-                    'page'  : page
-        }
-        return self.get_response(payload)
+def get_user_weekly_chart_list(self, user):
+    """
+    user.getWeeklyChartList
+    """
+    payload = {
+        'method' : 'user.getWeeklyChartList',
+        'user'   : user
+    }
+    return __get_response(payload)
 
-    def user_top_albums(self, user, limit=50, page=1):
-        payload = { 'method': 'user.getTopAlbums',
-                    'user'  : user,
-                    'limit' : limit,
-                    'page'  : page
-        }
-        return self.get_response(payload)
+def get_user_weekly_artists_chart(self, user, start=0, end=0):
+    """
+    user.getWeeklyArtistChart
+    """
+    payload = {
+        'method' : 'user.getWeeklyArtistChart',
+        'user'   : user,
+        'from'   : start,
+        'to'     : end
+    }
+    return __get_response(payload)
 
-    def user_top_tracks(self, user, limit=50, page=1):
-        payload = { 'method': 'user.getTopTracks',
-                    'user'  : user,
-                    'limit' : limit,
-                    'page'  : page
-        }
-        return self.get_response(payload)
+def get_user_weekly_albums_chart(self, user, start=0, end=0):
+    """
+    user.getWeeklyAlbumChart
+    """
+    payload = {
+        'method' : 'user.getWeeklyAlbumChart',
+        'user'   : user,
+        'from'   : start,
+        'to'     : end
+    }
+    return __get_response(payload)
 
-    def user_weekly_chart_list(self, user):
-        payload = { 'method': 'user.getWeeklyChartList',
-                    'user'  : user
-        }
-        return self.get_response(payload)
+def get_user_weekly_tracks_chart(self, user, start=0, end=0):
+    """
+    user.getWeeklyTrackChart
+    """
+    payload = {
+        'method' : 'user.getWeeklyTrackChart',
+        'user'   : user,
+        'from'   : start,
+        'to'     : end
+    }
+    return __get_response(payload)
 
-    def user_weekly_artists_chart(self, user, start=0, end=0):
-        payload = { 'method': 'user.getWeeklyArtistChart',
-                    'user'  : user,
-                    'from'  : start,
-                    'to'    : end
-        }
-        return self.get_response(payload)
+#=====================================================#
+#----------------| Private functions |----------------#
+#=====================================================#
 
-    def user_weekly_albums_chart(self, user, start=-1, end=-1):
-        payload = { 'method': 'user.getWeeklyAlbumChart',
-                    'user'  : user,
-                    'from'  : start,
-                    'to'    : end
-        }
-        return self.get_response(payload)
+def __get_response(self, payload):
+    """
+    Appends API key and format to the payload and returns the formatted API Response
+    """
+    global lastrequesttime
 
-    def user_weekly_tracks_chart(self, user, start=-1, end=-1):
-        payload = { 'method': 'user.getWeeklyTrackChart',
-                    'user'  : user,
-                    'from'  : start,
-                    'to'    : end
-        }
-        return self.get_response(payload)
+    payload['api_key'] = API_KEY
+    payload['format']  = 'json'
+
+    __rate_limiter()
+    response = requests.get(API_URL, headers=HEADERS, params=payload)
+
+    if USE_CACHE and 'from_cache' not in response:
+        lastrequesttime = time.time() # Update time of last API call
+
+    if response.status_code == requests.codes.ok:
+        return response.json()
+    else:
+        return { 'response error': response.text }
+
+def __rate_limiter(self):
+    """
+    Waits until the required interval between API requests is reached
+    """
+    if lastrequesttime is not None: # If there was a previous call to the API
+        timesince = time.time() - lastrequesttime
+        if timesince < CALL_INTERVAL:
+            time.sleep(CALL_INTERVAL - timesince)
