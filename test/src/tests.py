9cc9006dd206daa48bfe27bf01bb028fc8e79fff
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-08-22 04:20:39 -0700

    test_api_call

--- test/src/tests.py
+++ test/src/tests.py
@@ -8,9 +8,9 @@ class Tests(unittest.TestCase):
     def test_nothing(self):
         self.assertFalse(False)
 
-    def test_user_info(self):
+    def test_api_call(self):
         r = lastfmget.user_info('D3r3k523')
-        print(r)
+        self.assertEqual(r['user']['name'], 'D3r3k523')
 
 if __name__ == '__main__':
     lastfmget.init('test/api_cfg.yaml')
