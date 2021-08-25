1b6cb215e460164e749116cda9fd9d311ed52c1d
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-08-25 14:27:10 -0700

    5

--- scripts/actions/test_release/setupenv.sh
+++ scripts/actions/test_release/setupenv.sh
@@ -5,6 +5,6 @@ call_rate=5
 enable_cache=false
 
 pip install --disable-pip-version-check -qqq pyyaml
-python3 scripts/gen_api_cfg_yaml.py $cfg_fn $api_url $TEST_API_KEY $user_agent $call_rate $enable_cache
+python3 scripts/gen_api_cfg_yaml.py $cfg_fn $api_url $TEST_API_KEY $user_agent 5 $enable_cache
 
 pip install --upgrade --disable-pip-version-check -q lastfmget
