af3332edc1e37a9ffcc8e48e8eaf5153ead9b651
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-08-25 14:26:27 -0700

    fixed?

--- scripts/actions/test_release/setupenv.sh
+++ scripts/actions/test_release/setupenv.sh
@@ -1,9 +1,9 @@
 cfg_fn=test/api_cfg.yaml
 api_url=http://ws.audioscrobbler.com/2.0/
 user_agent=D3r3k23
-call_rate=10
+call_rate=5
 enable_cache=false
-echo $call_rate
+
 pip install --disable-pip-version-check -qqq pyyaml
 python3 scripts/gen_api_cfg_yaml.py $cfg_fn $api_url $TEST_API_KEY $user_agent $call_rate $enable_cache
 
