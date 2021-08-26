9a54fc2025f936377dc3da97c7f48f284d3f42e7
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-08-25 18:26:22 -0700

    Ok

--- scripts/gen_docs.bat
+++ scripts/gen_docs.bat
@@ -3,5 +3,3 @@
 pip install --upgrade --disable-pip-version-check -qqq pdoc
 
 python -m pdoc -t docs\templates -o docs\pdoc src\lastfmget
-
-@REM del docs\pdoc\index.html
--- scripts/gen_docs.sh
+++ scripts/gen_docs.sh
@@ -1,5 +1,3 @@
 pip install --upgrade --disable-pip-version-check -qqq pdoc
 
 python3 -m pdoc -t docs/templates -o docs/pdoc src/lastfmget
-
-rm docs/pdoc/index.html
