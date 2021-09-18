fb084640914b2644eb08995f0e743a015c328912
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-09-18 16:59:15 -0700

    Moved files

--- scripts/actions/gen_cfg.sh
+++ scripts/actions/gen_cfg.sh
@@ -7,5 +7,5 @@ call_rate=5
 enable_cache=false
 
 pip install --disable-pip-version-check -qqq pyyaml
-python3 scripts/actions/gen_api_cfg_yaml.py $cfg_fn_no_cache   $api_url $TEST_API_KEY $user_agent $call_rate $enable_cache
-python3 scripts/actions/gen_api_cfg_yaml.py $cfg_fn_with_cache $api_url $TEST_API_KEY $user_agent $call_rate $enable_cache
+python3 scripts/gen_api_cfg_yaml.py $cfg_fn_no_cache   $api_url $TEST_API_KEY $user_agent $call_rate $enable_cache
+python3 scripts/gen_api_cfg_yaml.py $cfg_fn_with_cache $api_url $TEST_API_KEY $user_agent $call_rate $enable_cache
