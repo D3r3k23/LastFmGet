d49f2487a4476f2d9ed140af9a3a69ccbefc3328
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-09-18 00:20:01 -0700

    dumps

--- test/dump.py
+++ test/dump.py
@@ -1,31 +1,64 @@
 import lastfmget
 
 import json
+from dataclasses import dataclass, field
+from typing import Callable
+
+USER = 'D3r3k523'
+
+@dataclass
+class DUMP:
+    name: str
+    meth: Callable
+    args: dict = field(default_factory=dict)
 
 def main():
     lastfmget.init('cfg/api_cfg_with_cache.yaml')
 
-    USER = 'D3r3k523'
-    methods = [
-        ( lastfmget.user_info,                USER ),
-        ( lastfmget.user_now_playing,         USER ),
-        ( lastfmget.user_recent_tracks,       USER ),
-        ( lastfmget.user_top_artists,         USER ),
-        ( lastfmget.user_top_albums,          USER ),
-        ( lastfmget.user_top_tracks,          USER ),
-        ( lastfmget.user_weekly_chart_list,   USER ),
-        ( lastfmget.user_weekly_artist_chart, USER ),
-        ( lastfmget.user_weekly_album_chart,  USER ),
-        ( lastfmget.user_weekly_track_chart,  USER )
+    dumps = [
+        DUMP(name=meth.__name__, meth=meth) for meth in [
+            lastfmget.user_info,
+            lastfmget.user_now_playing,
+            lastfmget.user_recent_tracks,
+            lastfmget.user_top_artists,
+            lastfmget.user_top_albums,
+            lastfmget.user_top_tracks,
+            lastfmget.user_weekly_chart_list,
+            lastfmget.user_weekly_artist_chart,
+            lastfmget.user_weekly_album_chart,
+            lastfmget.user_weekly_track_chart
+        ]
+    ] + [
+        DUMP(name='user_recent_tracks_large',
+             meth=lastfmget.user_recent_tracks,
+             args={'count': 500}
+        ),
+        DUMP(name='user_top_artists_large',
+             meth=lastfmget.user_top_artists,
+             args={'count': 1000}
+        ),
+        DUMP(name='user_top_artists_7day',
+             meth=lastfmget.user_top_artists,
+             args={'count': 20, 'period': '7day'}
+        ),
+        DUMP(name='user_top_artists_1month',
+             meth=lastfmget.user_top_artists,
+             args={'count': 50, 'period': '1month'}
+        ),
+        DUMP(name='user_top_artists_12month',
+             meth=lastfmget.user_top_artists,
+             args={'count': 100, 'period': '12month'}
+        )
     ]
 
-    for method, *args in methods:
-        dump(method, *args)
+    for d in dumps:
+        dump(d)
 
-def dump(method, *args):
-    data = method(*args)
+def dump(d):
+    fn = f'dump/{d.name}.json'
+    data = d.meth(USER, **d.args)
 
-    with open(f'dump/{method.__name__}.json', 'w') as f:
+    with open(fn, 'w') as f:
         json.dump(data, f, indent=2)
 
 if __name__ == '__main__':
