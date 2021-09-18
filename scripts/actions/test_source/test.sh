e5c51deb8712604d5548f24ad15d5fba41cfd9f6
Derek Henderson <d3r3k523@gmail.com>
2021-09-17 20:37:32 -0700

    fixed actions

--- scripts/actions/test_source/test.sh
+++ scripts/actions/test_source/test.sh
@@ -1,12 +1,12 @@
 cd test
 
 echo Running tests without cache
-venv/bin/python3 test --use_src
+python3 test --use_src
 ret_code=$?
 
 if [ $ret_code -eq 0 ]; then
     echo Running tets with cache
-    venv/bin/python3 test --use_src --use_cache
+    python3 test --use_src --use_cache
     ret_code=$?
 fi
 
