17f136cc4243d65bed3d902c3b28b0090f6f9096
Derek Henderson <d3r3k523@gmail.com>
2021-09-29 15:00:00 -0700

    Updated build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,10 @@
-if [ "$1" != "-notest" ]; then
+if [ "$1" != "--notest" ]; then
     scripts/test.sh --failfast
     if [ $? -ne 0 ]; then exit 1 fi
 fi
 
 scripts/gen_docs.sh
+if [ $? -ne 0 ]; then exit 1 fi
 
 if [ -d dist ]; then rm -rf dist; fi
 
