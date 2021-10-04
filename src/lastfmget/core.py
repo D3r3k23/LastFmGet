544691a13ddddb29fb3060fb13917c006741bdea
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2021-10-04 03:04:33 -0700

    docstring again

--- src/lastfmget/core.py
+++ src/lastfmget/core.py
@@ -81,11 +81,13 @@ def __get_response(payload, firstattempt=True):
     * Raises exceptions for known Last.fm errors and requests exceptions otherwise
 
     Arguments:
-      payload:
+      * payload:
+      ```
       {
         'method' (str) -- Last.fm API method name
         params -- Last.fm API method params
       }
+      ```
 
     Returns:
       Dict with response data
